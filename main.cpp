df1c394423ac38760889ff825cfe5bec3d3b6c87
Nicolás Almaraz <nnico.almaraz@gmail.com>
2024-06-21 22:50:13 +0000

    Agrego comentario de explicacion

--- main.cpp
+++ main.cpp
@@ -10,3 +10,13 @@ int main()
         LD1 = B1_USER;
     }
 }
+
+/***
+Explicacion y apuntes del video
+
+-> File Clone -> Pones un repo que quieras (tiene que ser tuyo sino no te va a dejar escribir nada).
+-> Para commitear tenes que agregar un "stage" yendo a Changes -> (+) Esto va  a añadir una nueva instancia para cargar cambios
+
+-> Ponemos un nombre de commit. En este caso "Agrego comentario de explicacion"
+-> Voy a "Source Control: Git" y le doy al ✓
+*/
